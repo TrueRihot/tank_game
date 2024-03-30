f0fcab4c7611f3a67923d8ed47e285e2fab7bfb8
moritztheil <moritz@startup-werk.de>
2024-03-30 21:50:25 +0100

    better tank handling

--- src/3d/entities/tank.tsx
+++ src/3d/entities/tank.tsx
@@ -1,7 +1,7 @@
 import { Box, RoundedBox, useKeyboardControls } from "@react-three/drei";
 import { useControls } from "leva";
 import { Vector3 } from "three";
-import { useBox } from "@react-three/p2";
+import { Duplet, useBox } from "@react-three/p2";
 import { useFrame } from "@react-three/fiber";
 import { useEffect } from "react";
 
@@ -19,25 +19,57 @@ export const Tank = () => {
     tankScaleZ: tankBaseScale.z,
   });
 
+  const propellantPositionLeft: Duplet = [0, -tankScaleX];
+  const propellantPositionRight: Duplet = [0, tankScaleX];
+
   let angle = 0;
 
   const [, get] = useKeyboardControls();
+  const { force, linearDamping, angularDamping, angularRatio } = useControls("tanks", {
+    force: {
+      value: 5,
+      step: 1,
+      min: 1,
+      max: 100,
+    },
+    linearDamping: {
+      value: 0.9,
+      step: 0.01,
+      min: 0.1,
+      max: 1,
+    },
+    angularDamping: {
+      value: 0.9,
+      step: 0.01,
+      min: 0.1,
+      max: 1,
+    },
+    angularRatio: {
+      value: 1,
+      step: 1,
+      min: 1,
+      max: 5,
+    },
+  });
 
   useEffect(() => {
     api.angle.subscribe((res) => {
       angle = res;
     });
-    api.linearDamping.set(.4);
-    api.angularDamping.set(.4);
   }, []);
+
+  useEffect(() => {
+    api.linearDamping.set(linearDamping);
+    api.angularDamping.set(angularDamping);
+  }, [linearDamping, angularDamping]);
 
   const [ref, api] = useBox(() => (
     {
       mass: 10,
       position: [0, 0],
       args: [tankScaleX, tankScaleZ],
-      angularDamping: 1,
-      linearDamping: 1,
+      angularDamping: 11,
+      linearDamping: 11,
       onCollide: (e) => {
         console.log("collide", e);
       },
@@ -51,20 +83,23 @@ export const Tank = () => {
     const { forward, backward, left, right } = get();
     if (forward) {
       //move forward in the direction of the angle
-      api.applyImpulse([-(Math.sin(angle)) * 0.1, (Math.cos(angle)) * 0.1], [0, 0]);
-      console.log(angle);
+      api.applyLocalForce([force, 0], propellantPositionLeft);
+      api.applyLocalForce([force, 0], propellantPositionRight);
     }
     if (backward) {
       //move backward in the opposite direction of the angle
-      api.applyImpulse([(Math.sin(angle)) * 0.1, -(Math.cos(angle)) * 0.1], [0, 0]);
+      api.applyLocalForce([-force, 0], propellantPositionLeft);
+      api.applyLocalForce([-force, 0], propellantPositionRight);
     }
     if (left) {
       //rotate left
-      api.angle.set(angle - 0.1);
+      api.applyLocalForce([-angularRatio / force, 0], propellantPositionLeft);
+      api.applyLocalForce([angularRatio / force, 0], propellantPositionRight);
     }
     if (right) {
       //rotate right
-      api.angle.set(angle + 0.1);
+      api.applyLocalForce([angularRatio / force, 0], propellantPositionLeft);
+      api.applyLocalForce([-angularRatio / force, 0], propellantPositionRight);
     }
   });
 
@@ -72,21 +107,23 @@ export const Tank = () => {
   return (
     <>
       <group position={[0, 0, 0]} ref={ref}>
-        <RoundedBox castShadow position={[0, tankScaleY / 2, 0]}
-                    scale={[tankScaleX, tankScaleY, tankScaleZ]}
-                    bevelSegments={4}>
-          <meshStandardMaterial color="orange" />
-        </RoundedBox>
-        <RoundedBox
-          scale={[tankScaleX / 1.9, tankScaleY / 1.5, tankScaleX / 1.9]}
-          bevelSegments={4}
-          position={[0, tankScaleY + tankScaleY / 1.5 / 2, 0]}>
-          <meshStandardMaterial color="blue" />
-        </RoundedBox>
-        <Box scale={[tankScaleX / 7, tankScaleY / 3.6, tankScaleZ / 1.5]}
-             position={[0, tankScaleY + tankScaleY / 3.5, tankScaleZ / 2]}>
-          <meshStandardMaterial color="red" />
-        </Box>
+        <group rotation-y={Math.PI / 2}>
+          <RoundedBox castShadow position={[0, tankScaleY / 2, 0]}
+                      scale={[tankScaleX, tankScaleY, tankScaleZ]}
+                      bevelSegments={4}>
+            <meshStandardMaterial color="orange" />
+          </RoundedBox>
+          <RoundedBox
+            scale={[tankScaleX / 1.9, tankScaleY / 1.5, tankScaleX / 1.9]}
+            bevelSegments={4}
+            position={[0, tankScaleY + tankScaleY / 1.5 / 2, 0]}>
+            <meshStandardMaterial color="blue" />
+          </RoundedBox>
+          <Box scale={[tankScaleX / 7, tankScaleY / 3.6, tankScaleZ / 1.5]}
+               position={[0, tankScaleY + tankScaleY / 3.5, tankScaleZ / 2]}>
+            <meshStandardMaterial color="red" />
+          </Box>
+        </group>
       </group>
     </>
   );
